21facdc73dc05515d8a7915b8ef88e7b247e0cb3
Abhay Chauhan <chauhan27.abhay@gmail.com>
2021-09-21 17:01:24 +0530

    explorext v2

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,8 +7,29 @@ import './Main.css'
 function Main({ tree, breadCrumb }) {
     const [editFile, setEditFile] = useState(false);
     const [fileData, setFileData] = useState({});
+    const [carr, setcArr] = useState([]);
 
-    let barr = breadCrumb.split("/").slice(1);
+    useEffect(() => {
+        let barr = breadCrumb.split("/").slice(1);
+        function getFiles(idx, barr, tree ){
+            if(tree == undefined || barr.length == 0) return;
+            for(let i=0; i<tree.length; i++){
+                if(tree[i].type == "folder"){
+                    if(tree[i].name == barr[idx]){
+                        if(idx == barr.length-1){
+                            setcArr(tree[i].childrens || []);
+                            return;
+                        }
+                        let childArr = tree[i].childrens;
+                        getFiles(idx+1, barr, childArr);
+                    }
+                }
+            }
+        }
+        getFiles(0, barr, tree)
+    }, [breadCrumb])
+
+    let farr = carr.filter(obj => obj.type !== "folder");
 
     const handleClick = (file) => {
         setFileData({
@@ -20,14 +41,14 @@ function Main({ tree, breadCrumb }) {
 
     return (
         <div className="main">
-            {/* {
-                filArr.map(file => (
+            {
+                farr.map(file => (
                     <div className="file" onClick={() => handleClick(file)}>
                         <img className="file-img" src={document} alt="document" />
                         <p className="file-name">{file.name}</p>
                     </div>
                 ))
-            } */}
+            }
             {editFile && <EditFile setEditFile={setEditFile} fileData={fileData} />}
         </div>
     )
